3892c0b66935d15747145c3703a2de26d316513b
Jimmy Thai <jvthai1@gmail.com>
2022-05-05 23:53:56 -0700

    Add beginning code. Fix HTML to show grid basis

---
+++ script.js
@@ -0,0 +1,20 @@
+//make default grid
+const container = document.querySelector('#container');
+
+/*
+function createGrid(rows, cols){
+   for (let i = 0; i < (rows * cols); i++){
+        container.style.setProperty('--grid-rows', rows);
+        container.style.setProperty('--grid-cols', cols);
+        let cell = document.createElement('div');
+        cell.innerText = (i + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
+ 
+}
+createGrid(16, 16)*/
+
+function createGrid(x, y){
+
+}
+ß
