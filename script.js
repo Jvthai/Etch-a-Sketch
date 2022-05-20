22aecd21dbf44a97782b26b0fc42b80745e72b96
Jimmy Thai <jvthai1@gmail.com>
2022-05-20 01:58:47 -0700

    Add on hover functionality and refined grid layout

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 //make default grid
 const container = document.querySelector('#container');
 
-
 function createGrid(rows, cols){
    for (let i = 0; i < (rows * cols); i++){
+    //establishes the base grid
         container.style.setProperty('--grid-rows', rows);
         container.style.setProperty('--grid-cols', cols);
         let cell = document.createElement('div');
-        cell.innerText = (i + 1);
         container.appendChild(cell).className = "grid-item";
-
+    //assigns the each cell the ability to change the color on hover
+        let gridItemArr = document.getElementsByClassName("grid-item");
+            gridItemArr[i].addEventListener("mouseover", function(){
+                gridItemArr[i].style.backgroundColor = "green";
+            });  
     };
- 
-}
-createGrid(16, 16)
+};
 
-// function createGrid(x, y){
-
-// }
+createGrid(16, 16);
