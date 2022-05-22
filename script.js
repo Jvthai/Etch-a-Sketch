7434a111c818a00829221bf0c58dd36acc25a2cd
Jimmy Thai <jvthai1@gmail.com>
2022-05-22 02:09:15 -0700

    Add reset button

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //establish DOM element container for the grid
 const container = document.querySelector('#container');
 
+const resetBtn = document.querySelector('#resetBtn'); 
+resetBtn.addEventListener("click", function(){
+    //remove the contents of the container somehow and then call createGrid function to recreate a new grid
+    function empty(element){
+        element.innerHTML = ""; 
+    }
+    empty(container);
+    createGrid(16,16);
+}); 
+
 function createGrid(rows, cols){
    for (let i = 0; i < (rows * cols); i++){
     //establishes the base grid
