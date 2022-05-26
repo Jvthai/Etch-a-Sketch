9e0de75d79603fa8c663ea840af6d9c613db2cd7
Jimmy Thai <jvthai1@gmail.com>
2022-05-25 22:48:55 -0700

    Add css styling to center content

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 //establish DOM element container for the grid
 const container = document.querySelector('#container');
 
+function empty(element){
+    element.innerHTML = ""; 
+}
+
 const resetBtn = document.querySelector('#resetBtn'); 
 resetBtn.addEventListener("click", function(){
     //remove the contents of the container somehow and then call createGrid function to recreate a new grid
-    function empty(element){
-        element.innerHTML = ""; 
-    }
     empty(container);
     createGrid(16,16);
 }); 
+
+//cutom grid button
+const customGrid = document.querySelector('#customGrid')
+customGrid.addEventListener("click", function(){
+    let num = prompt("Please enter a number for the grid");
+        empty(container);
+        createGrid(num, num);  
+});
 
 function createGrid(rows, cols){
    for (let i = 0; i < (rows * cols); i++){
